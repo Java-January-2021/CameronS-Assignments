75a8e25460e7b24a5081ec1326b44c7671983fab
Cameron Smith <theauspiciouscammy@gmail.com>
2021-01-15 15:38:39 -0800

    Deleted gorilla class from project

--- 02-java-oop/04-zoo-keeper-pt2/src/ZooKeeperTest.java
+++ 02-java-oop/04-zoo-keeper-pt2/src/ZooKeeperTest.java
@@ -2,14 +2,6 @@
 public class ZooKeeperTest {
 
 	public static void main(String[] args) {
-		Gorilla kingKong = new Gorilla();
-		kingKong.throwSomething();
-		kingKong.throwSomething();
-		kingKong.throwSomething();
-		kingKong.eatBananas();
-		kingKong.eatBananas();
-		kingKong.climb();
-		kingKong.displayEnergy();
 		Bat smaug = new Bat();
 		smaug.fly();
 		smaug.attackTown();
