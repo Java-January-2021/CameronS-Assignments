24f3e6d4d66b5c8430f891930340aa6a17608155
Cameron Smith <theauspiciouscammy@gmail.com>
2021-01-15 16:45:30 -0800

    assignment complete

---
+++ 02-java-oop/05-object-master-pt1/src/ObjectMasterTest.java
@@ -0,0 +1,12 @@
+
+public class ObjectMasterTest {
+
+	public static void main(String[] args) {
+		Human wiskeyjack = new Human();
+		Human fiddler = new Human();
+		wiskeyjack.attack(fiddler);
+		fiddler.displayHealth();
+
+	}
+
+}
