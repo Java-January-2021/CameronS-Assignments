5ae2b8b5bc9469bf4d9b1e418367d87cbd791052
Cameron Smith <theauspiciouscammy@gmail.com>
2021-01-20 11:04:58 -0800

    2nd commit for project setup

--- 02-java-oop/13-tries/src/com/tries/TrieController.java
+++ 02-java-oop/13-tries/src/com/tries/TrieController.java
@@ -3,7 +3,12 @@ package com.tries;
 public class TrieController {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Trie trie = new Trie();
+        trie.insertWord("car");
+        trie.insertWord("card");
+        trie.insertWord("chip");
+        trie.insertWord("trie");
+        trie.insertWord("try");
 
 	}
 
