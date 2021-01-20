0343f93f580e3a0577a263551df4c1411dcc7d4d
Cameron Smith <theauspiciouscammy@gmail.com>
2021-01-20 11:01:35 -0800

    Project set up complete

---
+++ 02-java-oop/13-tries/src/com/tries/Trie.java
@@ -0,0 +1,5 @@
+package com.tries;
+
+public class Trie {
+
+}
---
+++ 02-java-oop/13-tries/src/com/tries/TrieController.java
@@ -0,0 +1,10 @@
+package com.tries;
+
+public class TrieController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
